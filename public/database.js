1390030f1cc50dc70cdbafb6cdab68112c2764a9
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-11-09 14:34:12 -0800

    Added functionality for updating mongo database when server comes back online

--- public/database.js
+++ public/database.js
@@ -9,11 +9,36 @@ request.onupgradeneeded = function (event) {
 
 request.onsuccess = function (event) {
     db = event.target.result;
+
+    if (navigator.onLine) {
+        getData();
+    }
 }
 
 function saveRecord(data) {
     const transaction = db.transaction(["unsaved"], "readwrite");
-    const unSavedStore = transaction.objectStore(["unsaved"]);
+    const unSavedStore = transaction.objectStore("unsaved");
 
     unSavedStore.add(data);
 }
+
+function getData() {
+    const transaction = db.transaction(["unsaved"], "readwrite");
+    const unSavedStore = transaction.objectStore("unsaved");
+
+    const getAllData = unSavedStore.getAll();
+
+    getAllData.onsuccess = function () {
+        fetch("/api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAllData.result),
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json());
+    }
+}
+
+window.addEventListener("online", getData)
